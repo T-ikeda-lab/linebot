8a4e454244901fbf0dba558542a7ec9221a33355
T-ikeda-lab <a0015700u@yahoo.co.jp>
2024-10-13 16:28:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ load_dotenv()
 ## 環境変数を変数に割り当て
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 ## Flask アプリのインスタンス化
 app = Flask(__name__)
